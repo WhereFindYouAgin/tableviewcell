300697b7b118c76281864e643b9f54964ce50308
liuchao <excsir@163.com>
2015-09-15 09:17:56 +0800

    Initial Commit

---
+++ tableviewcell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableviewcell
+//
+//  Created by liuchao on 15/9/15.
+//  Copyright (c) 2015年 liuchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableviewcell/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tableviewcell
+//
+//  Created by liuchao on 15/9/15.
+//  Copyright (c) 2015年 liuchao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
